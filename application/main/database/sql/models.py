ca14055314834b42b76334b09948a104e6987aea
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-08-01 01:08:22 +0700

    Req. 1 - fix duplicate primary key error

--- application/main/database/sql/models.py
+++ application/main/database/sql/models.py
@@ -4,10 +4,10 @@ from application.main.database.sql.sqlite import Base
 
 
 class ChatDialogue(Base):
-    __tablename__ = "chat_history"
+    __tablename__ = "chat_dialogue"
 
     session_id = Column(String, primary_key=True, index=True)
-    sequence = Column(Integer, nullable=False, unique=False, index=False)
+    sequence = Column(Integer, primary_key=True, index=True)
     role = Column(String, nullable=False)
     content = Column(String, nullable=False)
 
