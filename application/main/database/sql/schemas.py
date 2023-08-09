055fbb326e45ffdd102051088ca615551c4159ca
Nguyen Cuong <36143842+ntcuong777@users.noreply.github.com>
2023-08-09 20:45:19 +0700

    Req. 2 - add service and router

--- application/main/database/sql/schemas.py
+++ application/main/database/sql/schemas.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
+from typing import List
 
 
+################# START: Model for chat dialogue function #################
 class ChatDialogueBase(BaseModel):
     session_id: str
     content: str
@@ -15,3 +17,46 @@ class ChatDialogue(ChatDialogueCreate):
 
     class Config:
         orm_mode = True
+
+################# END: Model for chat dialogue function #################
+
+
+################# START: Model for document QA function #################
+# ----- Base model ------
+class DocumentBase(BaseModel):
+    session_id: str
+    doc_content: str
+
+
+class DocumentEmbeddingsBase(BaseModel):
+    doc_part: str
+    doc_part_idx: int
+    embedding_vector: str
+    document: DocumentBase
+
+
+# ----- Models for creation ------
+class DocumentCreate(DocumentBase):
+    pass
+
+
+class DocumentEmbeddingsCreate(DocumentBase):
+    pass
+
+
+# ----- Models for reading ------
+class Document(DocumentCreate):
+    embeddings: List[DocumentEmbeddingsBase]
+
+    class Config:
+        orm_mode = True
+
+
+class DocumentEmbeddings(DocumentEmbeddingsCreate):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+################# START: Model for document QA function #################
---
+++ application/main/routers/document_question_answering.py
@@ -0,0 +1,34 @@
+from fastapi.routing import APIRouter
+from fastapi import Depends
+from sqlalchemy.orm import Session
+
+from application.main.database.sql.sqlite import get_db_session
+from application.main.database.sql.schemas import DocumentCreate, DocumentBase
+from application.main.services.document_question_answering_service import get_document_question_answering_service, DocumentQuestionAnsweringService
+from application.initializer import LoggerInstance
+
+router = APIRouter(prefix='/doc_qa')
+logger = LoggerInstance().get_logger(__name__)
+
+@router.post("/")
+def save_doc(
+        doc: DocumentCreate,
+        db: Session = Depends(get_db_session),
+        service: DocumentQuestionAnsweringService = Depends(get_document_question_answering_service)
+):
+    session_id = doc.session_id
+    doc_content = doc.doc_content
+    service.save_doc(db, session_id=session_id, doc_content=doc_content)
+    return "ok"
+
+
+@router.get("/{model}/{session_id}")
+def question_answering(
+        model: str,
+        session_id: str,
+        question: str,
+        db: Session = Depends(get_db_session),
+        service: DocumentQuestionAnsweringService = Depends(get_document_question_answering_service)
+):
+    assistant_response = service.get_assistant_response(db, model, session_id, question)
+    return {"assistant_response": assistant_response}
